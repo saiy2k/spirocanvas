979485b02fb9a71099da8e6dafa1345b51ae01d8
saiy2k@gmail.com <saiy2k@gmail.com@eed8afe5-ebb3-9951-b6dd-dbfba37d21cb>
2011-02-10 17:40:32 +0000

    1. added color pickers 2. documented lightly 3. code cleaned up a bit 4. applied GNU GPL V3

--- scripts/spiroCanvasCore.js
+++ scripts/spiroCanvasCore.js
@@ -1,3 +1,33 @@
+/*
+Copyright 2011 Saiyasodharan (http://saiy2k.blogspot.com/)
+
+This file is part of SpiroCanvas (http://code.google.com/p/spirocanvas/)
+
+SpiroCanvas is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SpiroCanvas is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SpiroCanvas.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+This class contains the core logic for drawing the spirographs.
+As of now, there are two types of curves supported:
+1. Epitrochoid
+2. Hypotrochoid
+There is a function named drawSpiro(), which will be invoked when
+the Redraw button is clicked, with all the required parameters.
+Then, based on the curveType, drawSpiro() calls either drawH() or
+drawE() in a loop to draw the curve.
+*/
+
 var spiroCanvasCore = (function()
 {
 	var my 			=	{};
@@ -8,11 +38,11 @@ var spiroCanvasCore = (function()
 	var aPlusb;
 	var aPlusbOverb;
 	var count;
-	var loopID;
+	var loopID		=	-1;
 	
+	//This function draws the Hypotrochoid
 	my.drawH 		=	function (ct, R, r, p, maxPoints, originPoint)
 	{
-	console.log("Draw H");
 		var newPoint=	{x:0, y:0};		
 		angle 		+=	angleStep;
 		newPoint.x 	=	originPoint.x + aMinusb * Math.cos(angle) + p * Math.cos(angle * aMinusbOverb);
@@ -22,75 +52,85 @@ var spiroCanvasCore = (function()
 		ct.stroke();
 		
 		count++;
-		if(count > maxPoints)
+			
+		if(count >= maxPoints)
+		{
+			ct.closePath();
 			self.clearInterval(loopID);
+			loopID 	=	-1;
+		}
 	};
 	
+	//This function draws the Epitrochoid
 	my.drawE 		=	function (ct, R, r, p, maxPoints, originPoint)
 	{
-		var newPoint	=	{x:0, y:0};
+		var newPoint=	{x:0, y:0};
 		angle += angleStep;
-		newPoint.x = originPoint.x + aPlusb * Math.cos(angle) - p * Math.cos(angle * aPlusbOverb);
-		newPoint.y = originPoint.y + aPlusb * Math.sin(angle) - p * Math.sin(angle * aPlusbOverb);
+		newPoint.x	=	originPoint.x + aPlusb * Math.cos(angle) - p * Math.cos(angle * aPlusbOverb);
+		newPoint.y	=	originPoint.y + aPlusb * Math.sin(angle) - p * Math.sin(angle * aPlusbOverb);
 		
-		ct.lineTo(newPoint.x, newPoint.y);
+		ct.lineTo(Math.floor(newPoint.x), Math.floor(newPoint.y));
 		ct.stroke();
 		
 		count++;
-		if(count > maxPoints)
+
+		if(count >= maxPoints)
+		{
+			ct.closePath();
 			self.clearInterval(loopID);
+			loopID 	=	-1;
+		}
 	};
-	
-	my.drawSpiro 		=	function (canvasEle)	
+
+	//This function is invoked when the Redraw button is pressed
+	my.drawSpiro 	=	function (canvasEle, curveType, speed, R, r, p, foreColor, bgColor)	
 	{
-		console.log("Draw Spiro");
-		var isHypo = false;	
-		var R = 60.0;
-		var r = 50.0;
-		var p = 60.0;
-		angle = 0;
-		var NumRevolutions;
-		var NumPoints;
-		var PointsPerCurve = 16;
-		var oldPoint	=	{x:0, y:0};
-		var ptOrigin	=	{x:0, y:0};
+		if(loopID != -1)
+		{
+			self.clearInterval(loopID);
+		}
+	
+		angle				=	0;
+		var NumRevolutions	=	0;
+		var NumPoints		=	0;
+		var PointsPerCurve	=	2;
+		var oldPoint		=	{x:0, y:0};
+		var ptOrigin		=	{x:0, y:0};
 		
 		var canvasBase		=	document.getElementById(canvasEle);
-		ptOrigin.x = canvasBase.width / 2;
-		ptOrigin.y = canvasBase.height / 2;
+		ptOrigin.x 			=	canvasBase.width / 2;
+		ptOrigin.y			=	canvasBase.height / 2;
+		angleStep			=	(Math.PI * 2) / PointsPerCurve;
+		NumRevolutions		=	r / MMath.HCF (R, r);
+		NumPoints			=	PointsPerCurve * NumRevolutions;
 		
-		angleStep = Math.PI * 2 / PointsPerCurve;
-
-		// Compute number of revolutions.
-		NumRevolutions = r/MMath.HCF (R, r);
-
-		// Total number of points to generate
-		NumPoints = PointsPerCurve * NumRevolutions;
-		
-		aMinusb = R - r;
-		aMinusbOverb = aMinusb / r;
-		aPlusb = R + r;
-		aPlusbOverb = aPlusb / r;
-		
-		count = 0;
+		aMinusb				=	R - r;
+		aMinusbOverb		=	aMinusb / r;
+		aPlusb				=	R + r;
+		aPlusbOverb			=	aPlusb / r;
+		count				=	0;
 		var	ct				=	canvasBase.getContext('2d');
-		if (isHypo)
+		ct.fillStyle		=	"#" + bgColor;
+		ct.strokeStyle		=	"#" + foreColor;
+		if (curveType == 0)
 		{
-			oldPoint.x = ptOrigin.x + R - r + p;
-			oldPoint.y = ptOrigin.y;
+			oldPoint.x		=	ptOrigin.x + R - r + p;
+			oldPoint.y		=	ptOrigin.y;
 			ct.clearRect(0, 0, canvasBase.width, canvasBase.height);
+			ct.fillRect(0, 0, canvasBase.width, canvasBase.height);
 			ct.beginPath();
 			ct.moveTo(oldPoint.x, oldPoint.y);
-			loopID = self.setInterval( function() { drawH(ct, R, r, p, NumPoints, ptOrigin); }, 50);
+			loopID			=	self.setInterval( function() { my.drawH(ct, R, r, p, NumPoints, ptOrigin); }, 1 + (10 - speed) * 5);
 		}
-		else
+		else if(curveType == 1)
 		{
-			oldPoint.x = ptOrigin.x + R + r - p;
-			oldPoint.y = ptOrigin.y;
+			oldPoint.x		=	ptOrigin.x + R + r - p;
+			oldPoint.y		=	ptOrigin.y;
 			ct.clearRect(0, 0, canvasBase.width, canvasBase.height);
+			ct.fillRect(0, 0, canvasBase.width, canvasBase.height);
 			ct.beginPath();
 			ct.moveTo(oldPoint.x, oldPoint.y);
-			loopID = self.setInterval( function() { my.drawE(ct, R, r, p, NumPoints, ptOrigin); }, 50);
+			loopID			=	self.setInterval( function() { my.drawE(ct, R, r, p, NumPoints, ptOrigin); }, 1 + (10 - speed) * 5);
 		}
 	};
 	
--- scripts/spiroCanvasUI.js
+++ scripts/spiroCanvasUI.js
@@ -1,3 +1,28 @@
+/*
+Copyright 2011 Saiyasodharan (http://saiy2k.blogspot.com/)
+
+This file is part of SpiroCanvas (http://code.google.com/p/spirocanvas/)
+
+SpiroCanvas is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SpiroCanvas is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SpiroCanvas.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+This class does the magic of converting plain divs to jqueryui controls
+and thus setting up the application UI. One and only function in this class
+is initUI and it will be called, when the body is loaded
+*/
+
 var spiroCanvasUI = (function()
 {
 	var my 			=	{};
@@ -5,9 +30,58 @@ var spiroCanvasUI = (function()
 	{
 		jQuery(function()
 		{
+			//moves the canvasDiv (inside which our canvas tag resides) to the center of the screen
 			$("#canvasDiv").center();
+			
+			//moves the floating toolbox (which includes all the sliders) to the left center of the screen
 			$("#toolBox").leftCenter();
 			
+			//click handler for reDraw Button. Calls the the drawSpiro function in spiroCanvasCore
+			//to draw a new spirograph
+			$( "#reDrawButton" ).click
+			(
+				function()
+				{
+					var R 			=	$('#circle1RadiusSlider').slider('value');
+					var r 			=	$('#circle2RadiusSlider').slider('value');
+					var p 			=	$('#pointDistanceSlider').slider('value');
+					var foreColor	=	$.jPicker.List[0].color.active.val('hex');
+					var bgColor		=	$.jPicker.List[1].color.active.val('hex');
+					var speed		=	$('#speedSlider').slider('value');
+					
+					spiroCanvasCore.drawSpiro('canvasBase', 1, speed, R, r, p, foreColor, bgColor);
+				}
+			);
+			
+			//transforms the foregroundColorDiv to a colorpicker
+			$('#foregroundColorDiv').jPicker
+			({
+				window:
+				{
+					expandable: true
+				},
+				color:
+				{
+					alphaSupport: true,
+					active: new $.jPicker.Color({ ahex: '000000ff' })
+				}
+			});
+			
+			//transforms the backgroundColorDiv to a colorpicker
+			$('#backgroundColorDiv').jPicker
+			({
+				window:
+				{
+					expandable: true
+				},
+				color:
+				{
+					alphaSupport: true,
+					active: new $.jPicker.Color({ ahex: 'ffffffff' })
+				}
+			});
+
+			//transforms the canvasDiv to a resizable panel with a fixed aspect ratio
 			$( "#canvasDiv" ).resizable
 			({
 				aspectRatio		:	4 / 3,
@@ -15,6 +89,9 @@ var spiroCanvasUI = (function()
 				minHeight		:	300
 			});
 			
+			//moves the canvasDiv to the center of the screen, whenever it's resized
+			//and also adjusts the size of canvasBase (the real canvas) to match the
+			//size of canvasDiv
 			$( "#canvasDiv" ).bind
 			(
 				"resize",	function(event, ui)
@@ -25,11 +102,13 @@ var spiroCanvasUI = (function()
 							}
 			);
 		
+			//transforms the circle1RadiusSlider into a slider with a specific min and
+			//max values. Also, the label is updated as and when the slider value is changed
 			$( "#circle1RadiusSlider" ).slider
 			({
 				orientation		:	"vertical",
 				range			:	"min",
-				min				:	0,
+				min				:	1,
 				max				:	100,
 				value			:	60,
 				slide			:	function( event, ui )
@@ -38,11 +117,13 @@ var spiroCanvasUI = (function()
 									}
 			});
 			
+			//transforms the circle2RadiusSlider into a slider with a specific min and
+			//max values. Also, the label is updated as and when the slider value is changed
 			$( "#circle2RadiusSlider" ).slider
 			({
 				orientation		:	"vertical",
 				range			:	"min",
-				min				:	0,
+				min				:	1,
 				max				:	100,
 				value			:	60,
 				slide			:	function( event, ui )
@@ -51,16 +132,33 @@ var spiroCanvasUI = (function()
 									}
 			});
 			
-			$( "#circle3RadiusSlider" ).slider
+			//transforms the pointDistanceSlider into a slider with a specific min and
+			//max values. Also, the label is updated as and when the slider value is changed
+			$( "#pointDistanceSlider" ).slider
 			({
 				orientation		:	"vertical",
 				range			:	"min",
-				min				:	0,
+				min				:	1,
 				max				:	100,
 				value			:	60,
 				slide			:	function( event, ui )
 									{
-										$( "#circle3RadiusLabel" ).html( ui.value );
+										$( "#pointDistanceLabel" ).html( ui.value );
+									}
+			});
+			
+			//transforms the speedSlider into a slider with a specific min and
+			//max values. Also, the label is updated as and when the slider value is changed
+			$( "#speedSlider" ).slider
+			({
+				orientation		:	"vertical",
+				range			:	"min",
+				min				:	1,
+				max				:	10,
+				value			:	5,
+				slide			:	function( event, ui )
+									{
+										$( "#speedLabel" ).html( ui.value );
 									}
 			});
 		});
